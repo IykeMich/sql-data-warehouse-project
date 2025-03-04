727258bc0cdc1e3cd1f6f762c8f82521961c12ec
Montana <97899453+IykeMich@users.noreply.github.com>
2025-03-04 15:09:59 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,43 @@
+/* ================================================
+   Script Purpose:
+   This script is designed to drop and recreate the 
+   'DataWareHouse1' database in SQL Server. It also 
+   creates three schemas: 'bronze', 'silver', and 
+   'gold' within the newly created database. 
+   This is useful for setting up a fresh environment 
+   for data warehousing purposes.
+   ================================================ */
 
+/* ================================================
+   WARNING:
+   This script will permanently delete the 'DataWareHouse1' 
+   database and all its contents. Ensure that you have 
+   backed up any important data before running this script. 
+   This action cannot be undone.
+   ================================================ */
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWareHouse1' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWareHouse1')
+BEGIN
+    ALTER DATABASE DataWareHouse1 SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWareHouse1;
+END;
+GO
+
+-- Create the "DataWareHouse1" database
+CREATE DATABASE DataWareHouse1;
+GO
+
+USE DataWareHouse1;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
